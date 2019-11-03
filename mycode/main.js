eac4cb9be8a1a8c1a0ac36c8e1caa5cf54843599
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-11-02 21:00:46 -0400

    Update main.js

--- mycode/main.js
+++ mycode/main.js
@@ -10,6 +10,7 @@ const roleRoom = require('role.room');
 const roleRanger = require('role.ranger');
 const mem_clear = require('mem_clear');
 const profiler = require('screeps-profiler');
+const util = require('creeps.utils');
 const Traveler = require('Traveler');
 require('prototype.spawn') ();
 require('prototype.creep') ();
@@ -21,50 +22,7 @@ module.exports.loop = function() {
 	global.mem_hack();
 	profiler.wrap(function() {
 		if (Object.keys(Game.flags).length != global.flagCount) {
-			global.reserveFlags = [];
-			global.maintainFlags = [];
-			global.claimFlags = [];
-			global.harvestFlags = [];
-			global.attackFlags = [];
-			for (let name in Game.flags) {
-				++global.flagCount;
-				let flag = Game.flags[name];
-				if (name.includes("Reserve")) {
-					global.reserveFlags.push(flag);
-				}
-				if (name.includes("Maintain")) {
-					global.maintainFlags.push(flag);
-				}
-				if (name.includes("Claim")) {
-					global.claimFlags.push(flag);
-				}
-				if (name.includes("Harvest")) {
-					global.harvestFlags.push(flag);
-				}
-				if (name.includes("Attack")) {
-					global.attackFlags.push(flag);
-				}
-			}
-			for (let name in Game.rooms) {
-				let room = Game.rooms[name];
-				room.memory.flagCount = 0;
-				//claim ownership of flags in rooms bordering if no master room
-				let rooms = Game.map.describeExits(room.name);
-				let reserve = global.reserveFlags;
-				let harvest = global.harvestFlags;
-				let flags = reserve.concat(harvest);
-				for (let flag of flags) {
-					if (!flag.memory.master) {
-						for (let key in rooms) {
-							let roomName = rooms[key];
-							if (flag.room.name == roomName) {
-								flag.memory.master = room.name;
-								++room.memory.flagCount;
-							}
-						}
-					}
-				}
-			}
+			util.intializeFlags();
 		}
 		//iterate through rooms and create the variables
 		for (let name in Game.rooms) {
