1e324ee0b07d7d5ba2b737820fd59130840d2295
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-11-05 16:30:57 -0500

    Update role.room.js

--- mycode/role.room.js
+++ mycode/role.room.js
@@ -11,7 +11,7 @@ const roleRoom = {
 		room.damStructures = _.filter(room.structures, (s) => s.hits/s.hitsMax < 0.8);
 		room.structByType = _.groupBy(room.structures, (s) => s.structureType);
 		room.links = room.structByType[STRUCTURE_LINK];
-		let storage = room.storage;
+		let storage = Game.rooms[room.name].storage;
 		room.storageLink = storage.pos.findClosestByRange(room.links);
 		[room.walls, room.infrastructure] = _.partition(room.damStructures, (s) => s.structureType == STRUCTURE_WALL || s.structureType == STRUCTURE_RAMPART);
 		room.walls = _.sortBy(room.walls, (s) => s.hits);
