fbfacd0d5e2f0dbc1d7e4d236894a88bf88a5a96
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-11-07 12:59:44 -0500

    Update role.hauler.js

--- mycode/role.hauler.js
+++ mycode/role.hauler.js
@@ -5,9 +5,22 @@ const roleHauler = {
 			creep.getTarget();
 		}
 		let flag = Game.flags[creep.memory.flag];
-		creep.travelTo(flag);
-		if (global.harvestFlags.includes(flag)) {
-
+		if (creep.store.getUsedCapacity() == 0 && flag.room != creep.room) {
+			creep.goTo(flag);
+		}
+		else {
+			if (flag.room == creep.room) {
+				creep.getEnergy();
+			}
+			let home = Game.rooms[creep.memory.home];
+			if (creep.store.getFreeCapacity() == 0 && creep.room != home) {
+				creep.goTo(home.controller);
+			} else {
+				let target = creep.pos.findClosestByRange(creep.room.links);
+				if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.goTo(target);
+				}
+			}
 		}
 	}
 }
